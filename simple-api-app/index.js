216c904a99a63c6223368280e24aa13f89406975
Nearchos Paspallis <NPaspallis@uclan.ac.uk>
2022-01-10 20:14:25 +0200

    Adding comments

--- simple-api-app/index.js
+++ simple-api-app/index.js
@@ -2,25 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/test', (req, res) => {
+app.get('/test', (req, res) => { // handle GET requests at '/test'
     res.status(200) // set the status code to 200 (OK)
         .setHeader('content-type', 'application/json') // body is JSON
         .send({message: 'Testing GET method'}); // note body is JSON, not text
 });
 
-app.post('/test', (req, res) => {
+app.post('/test', (req, res) => { // handle POST requests at '/test'
     res.status(200)
         .setHeader('content-type', 'application/json')
         .send({message: 'Testing POST method'});
 });
 
-app.put('/test', (req, res) => {
+app.put('/test', (req, res) => { // handle PUT requests at '/test'
     res.status(200)
         .setHeader('content-type', 'application/json')
         .send({message: 'Testing PUT method'});
 });
 
-app.delete('/test', (req, res) => {
+app.delete('/test', (req, res) => { // handle DELETE requests at '/test'
     res.status(200)
         .setHeader('content-type', 'application/json')
         .send({message: 'Testing DELETE method'});
